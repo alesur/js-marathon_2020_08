3a6090ee985659bfdbe2468f7f8cf00c7c1c0ea7
alesur <aleksandr.surkov@outlook.com>
2020-08-19 23:31:03 +0300

    Homework 2

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("Yello!");
+function formattedPhone(phone) {
+  let result = phone.charAt(0);
+
+  for (let i = 1; i < phone.length; i++) {
+    if (i === 2) {
+      result += " (";
+    }
+    if (i === 5) {
+      result += ") ";
+    }
+    if (i === 8 || i === 10) {
+      result += "-";
+    }
+    result += phone.charAt(i);
+  }
+  return result;
+}
+
+const phoneNumber = prompt("Enter phone number");
+
+alert(formattedPhone(phoneNumber)); // Formated to +x (xxx) xxx-xx-xx
